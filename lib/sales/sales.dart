84339b52fd0960ffa2e63a8635dd6dc966ca6c0c
Palash-Onehub <palash.rambhia.sg@gretail.com>
2024-10-29 14:42:35 +0000

    Integrated new plugin

---
+++ lib/sales/sales.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class Sales extends StatefulWidget {
+  const Sales({super.key});
+
+  @override
+  State<Sales> createState() => _SalesState();
+}
+
+class _SalesState extends State<Sales> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Sales Widget"),
+    );
+  }
+}
